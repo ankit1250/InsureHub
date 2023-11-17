8541e8c8f8b804c91084a7347cd4c4d41c5c9411
ankit1250 <64578165+ankit1250@users.noreply.github.com>
2023-11-16 23:32:02 -0800

    added insurance-app-gateway files

---
+++ insurance-app-gateway/src/main/java/com/example/insuranceappgateway/InsuranceAppGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.insuranceappgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceAppGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceAppGatewayApplication.class, args);
+	}
+
+}
---
+++ insurance-app-gateway/src/test/java/com/example/insuranceappgateway/InsuranceAppGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insuranceappgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceAppGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
